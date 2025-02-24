2c0a1846cf2807251c573eb6b8429a196c36b9f4
eielelle <144611397+eielelle@users.noreply.github.com>
2025-02-24 08:49:13 +0800

    moved directories

--- app/museum-preview/page.jsx
+++ app/museum-preview/page.jsx
@@ -1,9 +1,11 @@
-import Home from "./components/home";
+import Home from "./components/pages/home";
 
 export default function MuseumPreview() {
-    return <main className="bg-white h-screen text-black container mx-auto">
-        <div className="h-[300%]">
-            <Home />
-        </div>
-    </main> 
+  return (
+    <main className="bg-white h-screen text-black container mx-auto">
+      <div className="h-[300%]">
+        <Home />
+      </div>
+    </main>
+  );
 }
