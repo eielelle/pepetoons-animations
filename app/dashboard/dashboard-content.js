845d56cfe1538aaaf3a977510c17b579607fbbe0
Jmsmr <mjamesmar@gmail.com>
2025-02-26 14:34:01 +0800

    Add DAshboard

---
+++ app/dashboard/dashboard-content.js
@@ -0,0 +1,113 @@
+export default function dashboardcontent(){
+    return (
+        <div className="grid grid-cols-3 grid-rows-3 gap-4 p-8">
+            {/* 1 */}
+           <div className="flex flex-col gap-4 p-8 items-center text-center">
+                <div className="avatar">
+                    <div className="w-51 rounded-full">
+                        <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+                    </div>
+                </div>
+                <div className="text">
+                    <h1 className="text-3xl font-bold">Birth</h1>
+                </div>
+            </div>
+
+            {/* 2 */}
+            <div className="flex flex-col gap-4 p-8 items-center text-center">
+                <div className="avatar">
+                    <div className="w-51 rounded-full">
+                        <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+                    </div>
+                </div>
+                <div className="text">
+                    <h1 className="text-3xl font-bold">Family</h1>
+                </div>
+            </div>
+
+            {/* 3 */}
+            <div className="flex flex-col gap-4 p-8 items-center text-center">
+                <div className="avatar">
+                    <div className="w-51 rounded-full">
+                        <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+                    </div>
+                </div>
+                <div className="text">
+                    <h1 className="text-3xl font-bold">Childhood</h1>
+                </div>
+            </div>
+
+            {/* 4 */}
+            <div className="flex flex-col gap-4 p-8 items-center text-center">
+                <div className="avatar">
+                    <div className="w-51 rounded-full">
+                        <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+                    </div>
+                </div>
+                <div className="text">
+                    <h1 className="text-3xl font-bold">Education</h1>
+                </div>
+            </div>
+
+            {/* 5 */}
+            <div className="flex flex-col gap-4 p-8 items-center text-center">
+                <div className="avatar">
+                    <div className="w-51 rounded-full">
+                        <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+                    </div>
+                </div>
+                <div className="text">
+                    <h1 className="text-3xl font-bold">Personal & Romantic life</h1>
+                </div>
+            </div>
+
+            {/* 6 */}
+            <div className="flex flex-col gap-4 p-8 items-center text-center">
+                <div className="avatar">
+                    <div className="w-51 rounded-full">
+                        <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+                    </div>
+                </div>
+                <div className="text">
+                    <h1 className="text-3xl font-bold">Legacy & Influence</h1>
+                </div>
+            </div>
+
+            {/* 7 */}
+            <div className="flex flex-col gap-4 p-8 items-center text-center">
+                <div className="avatar">
+                    <div className="w-51 rounded-full">
+                        <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+                    </div>
+                </div>
+                <div className="text">
+                    <h1 className="text-3xl font-bold">Place He Visit</h1>
+                </div>
+            </div>
+
+            {/* 8 */}
+            <div className="flex flex-col gap-4 p-8 items-center text-center">
+                <div className="avatar">
+                    <div className="w-51 rounded-full">
+                        <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+                    </div>
+                </div>
+                <div className="text">
+                    <h1 className="text-3xl font-bold">Exile</h1>
+                </div>
+            </div>
+
+            {/* 9 */}
+            <div className="flex flex-col gap-4 p-8 items-center text-center">
+                <div className="avatar">
+                    <div className="w-51 rounded-full">
+                        <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+                    </div>
+                </div>
+                <div className="text">
+                    <h1 className="text-3xl font-bold">Arrest and Execution</h1>
+                </div>
+            </div>
+        </div>
+    )
+}
