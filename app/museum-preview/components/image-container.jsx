bb80b87f3a090e541956b3dc9c108bedbc8b61c3
eielelle <144611397+eielelle@users.noreply.github.com>
2025-02-26 10:37:03 +0800

    added assets

---
+++ app/museum-preview/components/image-container.jsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function ImageContainer({ image }) {
+  return (
+    <div className="w-full h-full relative">
+        <div className="absolute w-full h-full top-0 left-0 bg-black opacity-60"></div>
+        <Image className="h-full w-full object-cover" src={image} alt="picture" />
+    </div>
+  );
+}
--- app/museum-preview/components/pages/gallery.jsx
+++ app/museum-preview/components/pages/gallery.jsx
@@ -1,3 +1,7 @@
+// DONT USE THIS PAGE
+// FOR ARCHIVING USE ONLY
+
+
 "use client";
 
 import Header from "../header";
@@ -5,35 +9,6 @@ import { useEffect, useState } from "react";
 import anime from "animejs";
 
 export default function Gallery() {
-  const [scrollPos, setScrollPos] = useState(0);
-
-  // Update scroll position on scroll event
-  useEffect(() => {
-    const handleScroll = () => {
-      setScrollPos(window.scrollY);
-    };
-
-    window.addEventListener("scroll", handleScroll);
-
-    return () => {
-      window.removeEventListener("scroll", handleScroll); // Cleanup listener
-    };
-  }, []);
-
-  useEffect(() => {
-    const scrollLimit =
-      document.documentElement.scrollHeight - window.innerHeight;
-    const scrollPercentage = scrollPos / scrollLimit;
-    const squares = document.querySelectorAll(".square-gallery");
-
-    anime({
-      targets: squares,
-      translateX: scrollPercentage * 100,
-      color: "red",
-      easing: "easeOutQuad",
-      duration: 0, // Instant transition to keep it smooth
-    });
-  }, [scrollPos]);
 
   return (
     <section id="gallery" className="h-screen bg-black text-white sticky top-0">
@@ -45,16 +20,16 @@ export default function Gallery() {
 
         <div className="flex justify-between">
           <div className="border border-white p-8 h-[400px]">
-            <div className="square-gallery w-[200px] h-[200px] bg-gray-500"></div>
+            <div className="square-gallery-left w-[200px] h-[200px] bg-gray-500"></div>
           </div>
           <div className="border border-white p-8 h-[400px]">
-            <div className="square-gallery w-[200px] h-[200px] bg-white"></div>
+            <div className="square-gallery-left w-[200px] h-[200px] bg-white"></div>
           </div>
           <div className="border border-white p-8 h-[400px]">
-            <div className="square-gallery w-[200px] h-[200px] bg-white"></div>
+            <div className="square-gallery-right w-[200px] h-[200px] bg-white"></div>
           </div>
           <div className="border border-white p-8 h-[400px]">
-            <div className="square-gallery w-[200px] h-[200px] bg-gray-500"></div>
+            <div className="square-gallery-right w-[200px] h-[200px] bg-gray-500"></div>
           </div>
         </div>
       </div>
