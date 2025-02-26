cf26e618fa69f0e23a0278b2277a495f789bec34
Jmsmr <mjamesmar@gmail.com>
2025-02-26 16:01:50 +0800

    Add Detail

--- app/dashboard/dashboard-content.js
+++ app/dashboard/dashboard-content.js
@@ -1,112 +1,112 @@
 export default function dashboardcontent(){
     return (
-        <div className="grid grid-cols-3 grid-rows-3 gap-4 p-8">
-            {/* 1 */}
+        <div className="bg-gradient-to-br from-cyan-300 to-blue-700 grid grid-cols-3 grid-rows-3 gap-4 p-8 bg">
+            {/* Birth */}
            <div className="flex flex-col gap-4 p-8 items-center text-center">
                 <div className="avatar">
                     <div className="w-51 rounded-full">
                         <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
                     </div>
                 </div>
-                <div className="text">
-                    <h1 className="text-3xl font-bold">Birth</h1>
-                </div>
+                <button className="btn bg-blue-400 text-white text-3xl items-center text-center rounded-b-md border-[#005fd8]">
+                        Birth                                                  
+                </button>
             </div>
 
-            {/* 2 */}
+            {/* Family */}
             <div className="flex flex-col gap-4 p-8 items-center text-center">
                 <div className="avatar">
                     <div className="w-51 rounded-full">
                         <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
                     </div>
                 </div>
-                <div className="text">
-                    <h1 className="text-3xl font-bold">Family</h1>
-                </div>
+                <button className="btn bg-blue-400 text-white text-3xl items-center text-center rounded-b-md border-[#005fd8]">
+                        Family                                              
+                </button>
             </div>
 
-            {/* 3 */}
+            {/* Childhood */}
             <div className="flex flex-col gap-4 p-8 items-center text-center">
                 <div className="avatar">
                     <div className="w-51 rounded-full">
                         <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
                     </div>
                 </div>
-                <div className="text">
-                    <h1 className="text-3xl font-bold">Childhood</h1>
-                </div>
+                <button className="btn bg-blue-400 text-white text-3xl items-center text-center rounded-b-md border-[#005fd8]">
+                        Childhood                                           
+                </button>
             </div>
 
-            {/* 4 */}
+            {/* Education */}
             <div className="flex flex-col gap-4 p-8 items-center text-center">
                 <div className="avatar">
                     <div className="w-51 rounded-full">
                         <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
                     </div>
                 </div>
-                <div className="text">
-                    <h1 className="text-3xl font-bold">Education</h1>
-                </div>
+                <button className="btn bg-blue-400 text-white text-3xl items-center text-center rounded-b-md border-[#005fd8]">
+                        Education                                             
+                </button>
             </div>
 
-            {/* 5 */}
+            {/* Personal & Romantic life */}
             <div className="flex flex-col gap-4 p-8 items-center text-center">
                 <div className="avatar">
                     <div className="w-51 rounded-full">
                         <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
                     </div>
                 </div>
-                <div className="text">
-                    <h1 className="text-3xl font-bold">Personal & Romantic life</h1>
-                </div>
+                <button className="btn bg-blue-400 text-white text-2xl text-center rounded-b-md border-[#005fd8]">
+                        Personal & Romantic life                                                
+                </button>
             </div>
 
-            {/* 6 */}
+            {/* Legacy & Influence */}
             <div className="flex flex-col gap-4 p-8 items-center text-center">
                 <div className="avatar">
                     <div className="w-51 rounded-full">
                         <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
                     </div>
                 </div>
-                <div className="text">
-                    <h1 className="text-3xl font-bold">Legacy & Influence</h1>
-                </div>
+                <button className="btn bg-blue-400 text-white text-3xl items-center text-center rounded-b-md border-[#005fd8]">
+                        Legacy & Influence                                             
+                </button>
             </div>
 
-            {/* 7 */}
+            {/* Place He Visit */}
             <div className="flex flex-col gap-4 p-8 items-center text-center">
                 <div className="avatar">
                     <div className="w-51 rounded-full">
                         <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
                     </div>
                 </div>
-                <div className="text">
-                    <h1 className="text-3xl font-bold">Place He Visit</h1>
-                </div>
+                <button className="btn bg-blue-400 text-white text-3xl items-center text-center rounded-b-md border-[#005fd8]">
+                        Place He Visit                                                 
+                </button>
             </div>
 
-            {/* 8 */}
+            {/* Exile */}
             <div className="flex flex-col gap-4 p-8 items-center text-center">
                 <div className="avatar">
                     <div className="w-51 rounded-full">
                         <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
                     </div>
                 </div>
-                <div className="text">
-                    <h1 className="text-3xl font-bold">Exile</h1>
-                </div>
+                <button className="btn bg-blue-400 text-white text-3xl items-center text-center rounded-b-md border-[#005fd8]">
+                        Exile                                                
+                </button>
             </div>
 
-            {/* 9 */}
+            {/* Arrest and & Execution */}
             <div className="flex flex-col gap-4 p-8 items-center text-center">
                 <div className="avatar">
                     <div className="w-51 rounded-full">
                         <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
                     </div>
                 </div>
-                <div className="text">
-                    <h1 className="text-3xl font-bold">Arrest and Execution</h1>
-                </div>
+                <button className="btn bg-blue-400 text-white text-3xl items-center text-center rounded-b-md border-[#005fd8]">
+                        Arrest and & Execution                                             
+                </button>
             </div>
         </div>
     )
