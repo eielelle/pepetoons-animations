933daa0aa52fce11d15e3bbee294757398c7155c
eielelle <144611397+eielelle@users.noreply.github.com>
2025-02-26 08:33:48 +0800

    added hook

--- app/museum-preview/components/description.jsx
+++ app/museum-preview/components/description.jsx
@@ -1,22 +1,28 @@
 "use client";
 
+import useAnimateScroll from "../../hooks/useAnimateScroll";
+import anime from "animejs";
 import { useEffect, useRef, useState } from "react";
-import { motion, useScroll, useTransform } from 'framer-motion';
 
-export default function Description() {
-  const targetRef = useRef(null);
+export default function Description({ parentHeight, scrollTop }) {
 
-  const { scrollYProgress } = useScroll({
-    target: targetRef,
-  });
-  
-  // Use scroll position to interpolate the width
-  const width = useTransform(scrollYProgress, [0, 1], ['20%', '60%']);
+  useAnimateScroll(parentHeight, scrollTop, 60, (val) => {
+      const square = document.querySelector(".black-square")
+      val = Math.max(20, val)
 
+      console.log(val)
+
+      anime({
+        targets: square,
+        width: `${val}%`,
+        easing: "easeOutQuad",
+        duration: 0,
+      });
+  })
 
   return (
     <div className="h-[30%] w-full mt-5 flex gap-5 justify-between">
-      <motion.div ref={targetRef} style={{ width }} className="aspect-square w-[20%] bg-black black-square"></motion.div>
+      <div className="aspect-square w-[20%] bg-black black-square"></div>
       <div className="text-right w-[30%]">
         <h1 className="font-bold text-6xl mb-4">
           Life and Works of Rizal: Virtual Museum
