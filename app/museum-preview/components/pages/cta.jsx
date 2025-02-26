89210842233930896a935d659ea8eede30e4f0a8
eielelle <144611397+eielelle@users.noreply.github.com>
2025-02-26 15:32:25 +0800

    updated code

--- app/museum-preview/components/pages/cta.jsx
+++ app/museum-preview/components/pages/cta.jsx
@@ -5,7 +5,9 @@ export default function CTA() {
     <section id="cta" className="h-screen bg-black text-white sticky top-0">
       <div className="container mx-auto">
         <Header color="white" />
-        <div className="border-t-2 border-white"></div>
+        <div className="border-t-2 border-white">
+          
+        </div>
       </div>
     </section>
   );
