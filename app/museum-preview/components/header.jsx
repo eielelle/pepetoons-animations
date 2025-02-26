05391135ce9e889b852d6cb0f30ea6d1de13d175
eielelle <144611397+eielelle@users.noreply.github.com>
2025-02-26 15:32:25 +0800

    updated code

--- app/museum-preview/components/header.jsx
+++ app/museum-preview/components/header.jsx
@@ -2,7 +2,7 @@ export default function Header({ color = "black" }) {
 
     return <header className={`flex justify-between items-center pt-5 pb-2 text-${color}`}>
         <span className="logo flex items-center gap-3"><span className={`square bg-${color} inline-block`} style={{width: '30px', height: '30px'}}></span> Museum Preview</span>
-        <span>Section 1 / Section 2 / Section 3</span>
-        <span>pepetoons animations © 2025</span>
+        <span>Life and Works of Rizal: Virtual Museum</span>
+        <span className="text-white">pepetoons animations © 2025</span>
     </header>
 }
