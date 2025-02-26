6161334d4eb5ac12ecfc602a68c217e7f95178ee
eielelle <144611397+eielelle@users.noreply.github.com>
2025-02-26 10:37:03 +0800

    added assets

--- app/museum-preview/components/small-cluster.jsx
+++ app/museum-preview/components/small-cluster.jsx
@@ -1,9 +1,9 @@
 export default function SmallCluster() {
   return (
     <>
-      <div className="w-[20px] h-full border-r-2"></div>
-      <div className="w-[10px] h-full border-r-2"></div>
-      <div className="w-[5px] h-full border-r-2"></div>
+      <div className="w-[20px] h-full border-r-2 border-black bg-gray-700"></div>
+      <div className="w-[10px] h-full border-r-2 border-black bg-gray-600"></div>
+      <div className="w-[5px] h-full border-r-2 border-black bg-gray-400"></div>
     </>
   );
 }
