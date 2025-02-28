29ef6ea28d4715af10a8b592be6410e5dde15141
eielelle <144611397+eielelle@users.noreply.github.com>
2025-02-28 08:03:58 +0800

    updated code

--- app/museum-preview/components/pages/cta.jsx
+++ app/museum-preview/components/pages/cta.jsx
@@ -1,19 +1,16 @@
 'use client'
 
 import Header from "../header";
-import Lottie from "lottie-react"
-import ticketAnim from "../../../../assets/anims/ticket-anim.json"
 
 export default function CTA() {
   return (
     <section id="cta" className="h-screen bg-black text-white sticky top-0">
       <div className="container mx-auto flex flex-col h-full">
         <Header color="white" />
-        <div className="border-t-2 text-center border-white flex-1 flex justify-center items-center">
+        <div className="border-t-2 text-center h-full border-white flex-1 flex justify-center items-center">
           <div>
             <h1 className="text-6xl mb-4">Enter the museum today!</h1>
             <button className="btn btn-xl btn-wide rounded-4xl bg-blue-800">Let's go </button>
-            <Lottie animationData={ticketAnim} style={{ width: "30rem" }} loop={false} />
           </div>
         </div>
       </div>
