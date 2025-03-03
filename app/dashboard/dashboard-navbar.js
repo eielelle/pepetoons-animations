47af033977febe4d5bece7e558606ef0e46823d8
KaiKidThief <roxas.269269@meycauayan.sti.edu.ph>
2025-03-03 09:01:51 +0800

    ADD ICON IN NAVBAR

--- app/dashboard/dashboard-navbar.js
+++ app/dashboard/dashboard-navbar.js
@@ -1,15 +1,67 @@
-export default function dashboardnav(){
-    return (
-        <nav className="bg-blue-400 p-4 shadow-lg">
-            <div className="container mx-auto flex justify-between items-center">
-                <div className="flex justify-between text-white">Pepetoons</div>
-                <ul className="flex flex-1 gap-4 justify-center items-center">
-                    <li><a href="#" className="text-white hover:text-gray-300">Home</a></li>
-                    <li><a href="#" className="text-white hover:text-gray-300">Dashboard</a></li>
-                    <li><a href="#" className="text-white hover:text-gray-300">About</a></li>
-                    <li><a href="#" className="text-white hover:text-gray-300">contact</a></li>
-                </ul>
-            </div>
-        </nav>
-    )
+export default function dashboardnav() {
+  return (
+    <nav className="bg-blue-400 p-4 shadow-lg">
+      <div className="container mx-auto flex justify-between items-center">
+        <div className="flex justify-between text-white">Pepetoons</div>
+        <ul className="flex flex-1 gap-4 justify-center items-center">
+          <li>
+            <a
+              href="#"
+              className="text-white hover:text-gray-300 hover:text-gray-300 flex items-center"
+            >
+              <img
+                src="navbar-icons/home.png"
+                alt="Home"
+                className="mr-1"
+                style={{ width: "20px", height: "20px" }}
+              />
+              Home
+            </a>
+          </li>
+          <li>
+            <a
+              href="#"
+              className="text-white hover:text-gray-300 hover:text-gray-300 flex items-center"
+            >
+              <img
+                src="navbar-icons/binoculars.png"
+                alt="Home"
+                className="mr-1"
+                style={{ width: "20px", height: "20px" }}
+              />
+              Explore
+            </a>
+          </li>
+          <li>
+            <a
+              href="#"
+              className="text-white hover:text-gray-300 hover:text-gray-300 flex items-center"
+            >
+              <img
+                src="navbar-icons/information-button.png"
+                alt="Home"
+                className="mr-2"
+                style={{ width: "20px", height: "20px" }}
+              />
+              About
+            </a>
+          </li>
+          <li>
+            <a
+              href="#"
+              className="text-white hover:text-gray-300 hover:text-gray-300 flex items-center"
+            >
+              <img
+                src="navbar-icons/contact.png"
+                alt="Home"
+                className="mr-2"
+                style={{ width: "20px", height: "20px" }}
+              />
+              contact
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 }
