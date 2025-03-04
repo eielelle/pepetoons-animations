24344a1c83a033d97afcf3bd0a348fcf5765358a
KaiKidThief <roxas.269269@meycauayan.sti.edu.ph>
2025-03-04 10:11:16 +0800

    ADD LEGACY MODULE

---
+++ app/legacy/legacy-content.jsx
@@ -0,0 +1,76 @@
+import React from "react";
+
+export default function LegacyContent() {
+  return (
+    <div
+      className="relative min-h-screen w-full flex items-center justify-center"
+      style={{
+        backgroundImage: "url('/legacy-assets/legacy.webp')",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+      }}
+    >
+      <div className="absolute inset-0 bg-black opacity-40"></div>
+
+      {/* card container */}
+      <div className="z-10 p-6 rounded-lg w-full max-w-6xl items-center">
+        <h6
+          className="text-8xl text-center mb-25"
+          style={{ fontFamily: "'Tahoma', Sans-serif" }}
+        >
+          LEGACY & INFLUENCE
+        </h6>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-9 items-center justify-center">
+          <div
+            className="card w-76 backdrop-blur-sm shadow flex items-center justify-center"
+            style={{ backgroundColor: "rgba(255, 255, 255, 0.1)" }}
+          >
+            <div className="card-body flex flex-col items-center">
+              <img
+                src="/legacy-assets/monument.jpg"
+                alt="Rizal Monument"
+                className="object-cover rounded-sm"
+              />
+              <h2 className="card-title text-xl font-bold text-center mt-4">
+                National Hero Statue
+              </h2>
+            </div>
+          </div>
+
+          <div
+            className="card w-76 backdrop-blur-sm shadow flex items-center justify-center"
+            style={{ backgroundColor: "rgba(255, 255, 255, 0.1)" }}
+          >
+            <div className="card-body flex flex-col items-center">
+              <img
+                src="/legacy-assets/Philippine_independence.jpg"
+                alt="Philippine Independence"
+                className="object-cover rounded-sm"
+              />
+              <h2 className="card-title text-xl font-bold text-center mt-4">
+                Philippine Independence
+              </h2>
+            </div>
+          </div>
+
+          <div
+            className="card w-76 backdrop-blur-sm shadow flex items-center justify-center"
+            style={{ backgroundColor: "rgba(255, 255, 255, 0.1)" }}
+          >
+            <div className="card-body flex flex-col items-center">
+              <img
+                src="/legacy-assets/rizal_commoratives.jpg"
+                alt="Modern Day Commoratives"
+                className="object-cover rounded-sm"
+              />
+              <h2 className="card-title text-md font-bold text-center mt-4">
+                Modern Day Commoratives
+              </h2>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Dashboardnav from "./dashboard/dashboard-navbar";
 import Dashboardcontent from "./dashboard/dashboard-content";
+import Legacycontent from "./legacy/legacy-content";
 
 export default function Home() {
   return (
@@ -8,6 +9,10 @@ export default function Home() {
       {/*UNCOMMENT THIS IF YOU WANT TO SHOW THE DASHBOARD*/}
       {/*<Dashboardnav*/}
       {/*<Dashboardcontent/>*/}
+
+      {/*UNCOMMENT THIS IF YOU WANT TO SHOW THE LEGACY*/}
+      {/*<Dashboardnav*/}
+      {/*<Legacycontent />*/}
     </div>
   );
 }
