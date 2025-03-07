c769120d1df56d7a892a243055505e062d84d60c
eielelle <144611397+eielelle@users.noreply.github.com>
2025-03-07 15:21:52 +0800

    added about page

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,17 @@
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
-    '@tailwindcss/postcss': {},
+    '@tailwindcss/postcss': {
+      theme: {
+        // Some useful comment
+        extend: {
+          fontFamily: {
+            'protest': ["Protest Revolution", 'sans-serif'],
+            'anton': ['Anton', 'sans-serif'],
+          },
+        },
+      },
+    },
   },
 };
 
