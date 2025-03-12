e8b3cfe501d473d6cae30cfc14e812478c3b0b71
“Axelwaren” <axelwarenoleno@gmail.com>
2025-03-12 11:24:55 +0800

    Initial arrest features

---
+++ app/arrest/arrest.jsx
@@ -0,0 +1,35 @@
+export default function Arrest() {
+    return (
+        <>
+
+            {/* Start of arrest module */}
+            <div className="h-screen bg-[#101230]">
+                <div className="grid grid-flow-col h-auto grid-rows-4 gap-4 p-5">
+                    <div className="text-center my-2">
+                        <h1 className="text-4xl font-bold">Arrest, Trial Execution</h1>
+                    </div>
+
+                    <div class="flex justify-around gap-5 h-40">
+                        <div className="bg-[https://placehold.co/300x300] ">01</div>
+                        <div>02</div>
+                    </div>
+
+                    <div class="flex justify-around gap-5 h-40">
+                        <div>01</div>
+                        <div>02</div>
+                    </div>
+
+                    <div class="flex justify-around gap-5 h-40">
+                        <div>01</div>
+                        <div>02</div>
+                    </div>
+
+
+                </div>
+            </div>
+            {/* End of arrest module */}
+
+
+        </>
+    );
+}
