532e13fd5f0ca52cf7c4719a60121a020e5273a8
eielelle <144611397+eielelle@users.noreply.github.com>
2025-03-13 14:29:22 +0800

    FIX

--- app/visit/asia/page.jsx
+++ app/visit/asia/page.jsx
@@ -6,37 +6,37 @@ import Image from "next/image";
 export default function Asia() {
   return (
     <main className="grid grid-cols-4 grid-rows-4">
-      <div className="h-125 bg-amber-200 flex justify-center items-end">
+      <div className="h-124 bg-amber-200 flex justify-center items-end">
         <button className="btn absolute btn-sm shadow rounded-sm mb-4">
           see more
         </button>
       </div>
 
-      <div className="h-125 bg-blue-300 flex justify-center items-end">
+      <div className="h-124 bg-blue-300 flex justify-center items-end">
         <button className="btn btn-sm shadow rounded-sm mb-4">see more</button>
       </div>
 
-      <div className="h-125 bg-red-300 flex justify-center items-end">
+      <div className="h-124 bg-red-300 flex justify-center items-end">
         <button className="btn btn-sm shadow rounded-sm mb-4">see more</button>
       </div>
 
-      <div className="h-125 bg-green-300 flex justify-center items-end">
+      <div className="h-124 bg-green-300 flex justify-center items-end">
         <button className="btn btn-sm shadow rounded-sm mb-4">see more</button>
       </div>
 
-      <div className="h-125 bg-cyan-300 flex justify-center items-end">
+      <div className="h-124 bg-cyan-300 flex justify-center items-end">
         <button className="btn btn-sm shadow rounded-sm mb-4">see more</button>
       </div>
 
-      <div className="h-125 bg-yellow-300 flex justify-center items-end">
+      <div className="h-124 bg-yellow-300 flex justify-center items-end">
         <button className="btn btn-sm shadow rounded-sm mb-4">see more</button>
       </div>
 
-      <div className="h-125 bg-violet-300 flex justify-center items-end">
+      <div className="h-124 bg-violet-300 flex justify-center items-end">
         <button className="btn btn-sm shadow rounded-sm mb-4">see more</button>
       </div>
 
-      <div className="h-125 bg-fuchsia-300 flex justify-center items-end">
+      <div className="h-124 bg-fuchsia-300 flex justify-center items-end">
         <button className="btn btn-sm shadow rounded-sm mb-4">see more</button>
       </div>
     </main>
