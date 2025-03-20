3fb6c954e7dc902ba39d449e9024a18dfd36c394
Timan AlghieVic <timanalghievic@gmail.com>
2025-03-20 10:23:29 +0800

    updated Rizal Birth

--- app/RizalsBirth/page.jsx
+++ app/RizalsBirth/page.jsx
@@ -3,53 +3,66 @@ import React from "react";
 import Image from "next/image";
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-import Pic1 from "../../assets/Resources/Picture1.png";
-import Calamba from "../../assets/Resources/Picture2.png";
+import Calamba from "../../assets/Resources/calamba.png";
+import Food from "../../assets/Resources/Rizal breakfast or merienda.png";
+import Church from "../../assets/Resources/church.png";
+import Antipolo from "../../assets/Resources/Image of the Virgin of Antipolo.png";
+import Baptist from "../../assets/Resources/baptist.png";
 
 const items = [
   {
     id: 1,
-    image: Pic1,
-    text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-           Vivamus lacinia, purus non bibendum malesuada, orci tortor elementum erat.
-           Bibendum Lorem ipsum dolor sit amet, consectetur adipiscing elit.`,
-    style: "flex-col",
+    image: Calamba,
+    text: `His father, Don Francisco, a man of quiet strength and unwavering discipline, 
+    began each day before the sun, his prayers a gentle hum against the morning stillness. 
+    He was a man of the land, his hands familiar with the soil, his mind versed in the wisdom of books. 
+    His days were structured, a rhythm of work and rest, punctuated by the simple pleasures of batsoy
+    and salabat, a short siesta, and the lingering aroma of lojua at merienda.`,
     textStyle: "text-justify",
   },
   {
     id: 2,
-    image: Calamba,
-    text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-           Vivamus lacinia, purus non bibendum malesuada, orci tortor elementum erat.
-           Bibendum Lorem ipsum dolor sit amet, consectetur adipiscing elit.`,
-    style: "flex-col",
+    image: Food,
+    text: `His mother, Doña Teodora, was a force of nature, 
+    a woman of deep faith and unyielding spirit. 
+    Her devotion led her to church, even as her time drew near.`,
     textStyle: "tracking-wide",
   },
   {
     id: 3,
-    image: Pic1,
-    text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-           Vivamus lacinia, purus non bibendum malesuada, orci tortor elementum erat.
-           Bibendum Lorem ipsum dolor sit amet, consectetur adipiscing elit.`,
-    style: "flex-col",
+    image: Church,
+    text: `It was there, amidst the hushed whispers of confession, 
+    that a strange thing occurred. A baby's cry, a mournful wail, echoed through the church, 
+    startling the penitents and the priest, Father Rufino Collantes. 
+    Doña Teodora, her heart heavy with worry, was approached by an old woman who spoke of a "protesting"
+    child within her womb. The delivery was difficult, a labor that tested her strength. 
+    She vowed a pilgrimage to Antipolo, a promise born of fear and hope.`,
     textStyle: "text-center",
   },
   {
     id: 4,
-    image: Pic1,
-    text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-           Vivamus lacinia, purus non bibendum malesuada, orci tortor elementum erat.
-           Bibendum Lorem ipsum dolor sit amet, consectetur adipiscing elit.`,
-    style: "flex-col",
+    image: Antipolo,
+    text: `Three days later, on June 22nd, little Jose was baptized, 
+    his godfather Don Pedro Casañas witnessing the ceremony. 
+    Father Collantes, gazing upon the infant's large head, foretold a future of greatness.`,
     textStyle: "text-center",
   },
   {
     id: 5,
-    image: Pic1,
-    text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+    image: Baptist,
+    text: `Jose was a delicate child, "tiny, even puny," 
+    needing the tender care of a yaya. Yet, even as a toddler, his mind was sharp, his eyes alert. 
+    He learned the alphabet at two, taught by his mother, his first and most influential teacher. 
+    He found joy in the family's orchard, climbing trees and dreaming under the shade of the fruit-laden branches.`,
+    textStyle: "text-center",
+  },
+  {
+    id: 6,
+    image: Calamba,
+    text: `Lorem iy7loc
+    0 0psum dolor sit amet, consectetur adipiscing elit. 
            Vivamus lacinia, purus non bibendum malesuada, orci tortor elementum erat.
            Bibendum Lorem ipsum dolor sit amet, consectetur adipiscing elit.`,
-    style: "flex-col",
     textStyle: "text-center",
   },
 ];
@@ -58,10 +71,10 @@ const Birth = () => {
   return (
     <div className="min-h-screen p-8 font-sans flex flex-col ">
       {/* Title */}
-      <h1 className="text-left text-4x1 font-extrabold mb-10">Rizal's Birth</h1>
+      <h1 className="text-left text-4xl font-extrabold mb-10">Rizal's Birth</h1>
 
       {/* Carousel */}
-      <div className="relative w-full max-w-[1200px] mx-auto">
+      <div className="relative w-full max-w-[1500px] mx-auto">
         <Carousel
           showArrows={true}
           showThumbs={false}
@@ -81,7 +94,7 @@ const Birth = () => {
               <Image
                 src={item.image}
                 alt="Thumbnials"
-                className="w-full h-64 object-cover rounded-t-lg"
+                className="w-full h-full object-cover rounded-t-lg"
               />
 
               {/* Text */}
@@ -94,7 +107,7 @@ const Birth = () => {
 
         {/* Custom Navigation Buttons */}
         <div className="flex justify-center mt-3">
-          <button
+          {/* <button
             className="px-4 py-2 rounded-full shadow-lg hover:bg-blue-800 transition-all mx-2"
             onClick={() => document.querySelector(".control-prev").click()}
           >
@@ -105,7 +118,7 @@ const Birth = () => {
             onClick={() => document.querySelector(".control-next").click()}
           >
             ▶
-          </button>
+          </button> */}
         </div>
       </div>
     </div>
