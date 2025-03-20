e23d2ecfacaaf0c60fd3e256c3342167815139bd
Timan AlghieVic <timanalghievic@gmail.com>
2025-03-20 09:25:11 +0800

    Rizal Birht

--- app/RizalsBirth/page.jsx
+++ app/RizalsBirth/page.jsx
@@ -1,61 +1,55 @@
 "use client";
 import React from "react";
-import img from "next/image";
+import Image from "next/image";
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import Pic1 from "../../assets/Resources/Picture1.png";
+import Calamba from "../../assets/Resources/Picture2.png";
 
 const items = [
   {
     id: 1,
-    img: "../../assets/Resources/Picture1.png",
-    text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-           Vivamus lacinia, purus non bibendum malesuada, orci tortor elementum erat.
-           Bibendum Lorem ipsum dolor sit amet, consectetur adipiscing elit.`,
-    style: "flex-col",
-    imagePosition: "self-start",
-    textStyle: "text-justify",
-  },
-  {
-    id: 2,
     image: Pic1,
     text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
            Vivamus lacinia, purus non bibendum malesuada, orci tortor elementum erat.
            Bibendum Lorem ipsum dolor sit amet, consectetur adipiscing elit.`,
     style: "flex-col",
-    imagePosition: "self-center",
+    textStyle: "text-justify",
+  },
+  {
+    id: 2,
+    image: Calamba,
+    text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+           Vivamus lacinia, purus non bibendum malesuada, orci tortor elementum erat.
+           Bibendum Lorem ipsum dolor sit amet, consectetur adipiscing elit.`,
+    style: "flex-col",
     textStyle: "tracking-wide",
   },
   {
     id: 3,
-    image: "/image3.jpg",
+    image: Pic1,
     text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
            Vivamus lacinia, purus non bibendum malesuada, orci tortor elementum erat.
            Bibendum Lorem ipsum dolor sit amet, consectetur adipiscing elit.`,
     style: "flex-col",
-    imagePosition: "self-end",
     textStyle: "text-center",
   },
-
   {
     id: 4,
-    image: "/image3.jpg",
+    image: Pic1,
     text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
            Vivamus lacinia, purus non bibendum malesuada, orci tortor elementum erat.
            Bibendum Lorem ipsum dolor sit amet, consectetur adipiscing elit.`,
     style: "flex-col",
-    imagePosition: "self-end",
     textStyle: "text-center",
   },
-
   {
     id: 5,
-    image: "/image3.jpg",
+    image: Pic1,
     text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
            Vivamus lacinia, purus non bibendum malesuada, orci tortor elementum erat.
            Bibendum Lorem ipsum dolor sit amet, consectetur adipiscing elit.`,
     style: "flex-col",
-    imagePosition: "self-end",
     textStyle: "text-center",
   },
 ];
@@ -64,16 +58,16 @@ const Birth = () => {
   return (
     <div className="min-h-screen p-8 font-sans flex flex-col ">
       {/* Title */}
-      <h1 className="text-left text-4xl font-extrabold mb-10">Rizal's Birth</h1>
+      <h1 className="text-left text-4x1 font-extrabold mb-10">Rizal's Birth</h1>
 
       {/* Carousel */}
-      <div className="relative w-[1000px] mx-auto">
+      <div className="relative w-full max-w-[1200px] mx-auto">
         <Carousel
           showArrows={true}
           showThumbs={false}
           infiniteLoop={true}
           centerMode={true}
-          centerSlidePercentage={40}
+          centerSlidePercentage={50} // Adjusted slide size
           emulateTouch={true}
           dynamicHeight={false}
           showIndicators={false} // Hide dots
@@ -81,24 +75,17 @@ const Birth = () => {
           {items.map((item) => (
             <div
               key={item.id}
-              className={`relative w-full bg-gray-200 shadow-lg rounded-lg p-4 flex ${item.style} items-center transition-transform duration-300 hover:scale-105 focus-within:scale-105`}
+              className="relative w-full bg-gray-200 shadow-lg rounded-lg p-4 flex flex-col items-center transition-transform duration-300 hover:scale-105 focus-within:scale-105"
             >
-              {/* Blue Header */}
-              <div className="w-95 h-43 bg-blue-400"></div>
-
-              {/* Image Placement */}
-              <div className={`p-4 ${item.imagePosition}`}>
-                <img
-                  src={item.image}
-                  alt="Thumbnail"
-                  className="w-20 h-20 object-cover rounded-lg shadow-md border border-gray-300 transition-opacity duration-300 hover:opacity-80 focus:opacity-80"
-                />
-              </div>
+              {/* Image Header */}
+              <Image
+                src={item.image}
+                alt="Thumbnials"
+                className="w-full h-64 object-cover rounded-t-lg"
+              />
 
               {/* Text */}
-              <div
-                className={`p-4 w-full text-black font-bold text-sm uppercase leading-5 ${item.textStyle} transition-colors duration-300 hover:text-blue-600 focus:text-blue-600`}
-              >
+              <div className="p-4 w-full text-black font-bold text-sm uppercase leading-5 text-center transition-colors duration-300 hover:text-blue-600 focus:text-blue-600">
                 <p className="mb-2">{item.text}</p>
               </div>
             </div>
@@ -108,13 +95,13 @@ const Birth = () => {
         {/* Custom Navigation Buttons */}
         <div className="flex justify-center mt-3">
           <button
-            className=" px-4 py-2 rounded-full shadow-lg hover:bg-blue-800 transition-all mx-2"
+            className="px-4 py-2 rounded-full shadow-lg hover:bg-blue-800 transition-all mx-2"
             onClick={() => document.querySelector(".control-prev").click()}
           >
             ◀
           </button>
           <button
-            className=" px-4 py-2 rounded-full shadow-lg hover:bg-blue-800 transition-all mx-2"
+            className="px-4 py-2 rounded-full shadow-lg hover:bg-blue-800 transition-all mx-2"
             onClick={() => document.querySelector(".control-next").click()}
           >
             ▶
