28364fe72feb0fd0b56a0816dbea0a7218566df6
eielelle <144611397+eielelle@users.noreply.github.com>
2025-03-25 07:34:47 +0800

    added data

--- app/exile/data.js
+++ app/exile/data.js
@@ -1,9 +1,30 @@
 export const data = [
     {
+        title: "Rizal Monument - Singapore",
+        imgs: ["img1link", "img2link", 'img3link'],
+        description: "Dr. José Rizal visited Singapore four times, with his first visit in 1882 during a two-day stopover for sightseeing before departing for Europe on May 11. He returned on May 17, 1892, marking his last visit before his execution on December 30, 1896. To honor his visits, a memorial featuring a bronze medallion by a Filipino artist was unveiled near the Asian Civilizations Museum in 2005 by Singaporean President S.R. Nathan and the National Heritage Board on the anniversary of Rizal's birth.",
+        address: "7VP2+VX Singapore",
+        coordinates: [3.6578373629768572, 104.68740186636394]
+    },
+    {
+        title: "Rizal Monument - Madrid, Spain",
+        imgs: [],
+        description: "Madrid, where José Rizal spent much of his time in Europe from 1882 to 1891, honors him with a monument in Parque Santander along Avenida de Filipinas. Arriving in 1882 after studying at the University of Santo Tomas, Rizal described Madrid as a city blending European and Oriental influences. Despite the city's distractions, he remained dedicated to literary, artistic, and patriotic pursuits. Unveiled on December 5, 1996, the monument features a four-meter-high bronze statue by Filipino artist Florante Caedo, standing near the statue of South American hero Simón Bolívar, symbolizing Rizal's lasting connection to Spain.",
+        address: "7VP2+VX Singapore",
+        coordinates: [3.6578373629768572, 104.68740186636394]
+    },
+    {
+        title: "Rizal",
+        imgs: [],
+        description: "Dr. José Rizal visited Singapore four times, with his first visit in 1882 during a two-day stopover for sightseeing before departing for Europe on May 11. He returned on May 17, 1892, marking his last visit before his execution on December 30, 1896. To honor his visits, a memorial featuring a bronze medallion by a Filipino artist was unveiled near the Asian Civilizations Museum in 2005 by Singaporean President S.R. Nathan and the National Heritage Board on the anniversary of Rizal's birth.",
+        address: "7VP2+VX Singapore",
+        coordinates: [3.6578373629768572, 104.68740186636394]
+    },
+    {
         title: "",
         imgs: [],
         description: "Dr. José Rizal visited Singapore four times, with his first visit in 1882 during a two-day stopover for sightseeing before departing for Europe on May 11. He returned on May 17, 1892, marking his last visit before his execution on December 30, 1896. To honor his visits, a memorial featuring a bronze medallion by a Filipino artist was unveiled near the Asian Civilizations Museum in 2005 by Singaporean President S.R. Nathan and the National Heritage Board on the anniversary of Rizal's birth.",
         address: "7VP2+VX Singapore",
         coordinates: [3.6578373629768572, 104.68740186636394]
-    }
+    },
 ]
