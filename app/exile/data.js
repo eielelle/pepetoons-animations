fedd001b6fd829c3549187ff59ba584b705768cb
eielelle <144611397+eielelle@users.noreply.github.com>
2025-03-25 20:48:10 +0800

    refactor visit page

--- app/exile/data.js
+++ app/exile/data.js
@@ -14,16 +14,37 @@ export const data = [
         coordinates: [3.6578373629768572, 104.68740186636394]
     },
     {
-        title: "Rizal",
+        title: "Rizal Site Marker - Hong Kong",
         imgs: [],
-        description: "Dr. José Rizal visited Singapore four times, with his first visit in 1882 during a two-day stopover for sightseeing before departing for Europe on May 11. He returned on May 17, 1892, marking his last visit before his execution on December 30, 1896. To honor his visits, a memorial featuring a bronze medallion by a Filipino artist was unveiled near the Asian Civilizations Museum in 2005 by Singaporean President S.R. Nathan and the National Heritage Board on the anniversary of Rizal's birth.",
+        description: "In 1888, José Rizal left Manila for Hong Kong, where he spent a self-imposed exile. From December 1891 to June 1892, he lived with his family at No. 5 D'Aguilar St. and later at No. 2, Rednaxela Terrace, where he practiced as an ophthalmic surgeon. A historical marker by the Hong Kong Antiquities and Monuments Office commemorates his stay, recognizing him as the national hero of the Philippines.",
         address: "7VP2+VX Singapore",
         coordinates: [3.6578373629768572, 104.68740186636394]
     },
     {
-        title: "",
+        title: "Rizal's Bust - Japan",
         imgs: [],
-        description: "Dr. José Rizal visited Singapore four times, with his first visit in 1882 during a two-day stopover for sightseeing before departing for Europe on May 11. He returned on May 17, 1892, marking his last visit before his execution on December 30, 1896. To honor his visits, a memorial featuring a bronze medallion by a Filipino artist was unveiled near the Asian Civilizations Museum in 2005 by Singaporean President S.R. Nathan and the National Heritage Board on the anniversary of Rizal's birth.",
+        description: "One of the happiest periods in José Rizal's life was his 45-day stay in Japan from February 28 to April 13, 1888. He arrived in Yokohama on February 18 and later stayed at the Tokyo Hotel before moving to the Spanish Legation. Fascinated by Japanese culture, he immersed himself in its language, traditions, and arts. He admired the country's natural beauty, the politeness of its people, and the charm of Japanese women, even falling in love with Seiko Usui. A bust of Rizal now stands in Hibiya Park, marking the site of the Tokyo Hotel where he once stayed.",
+        address: "7VP2+VX Singapore",
+        coordinates: [3.6578373629768572, 104.68740186636394]
+    },
+    {
+        title: "Rizal's Bust - Orlando, Florida",
+        imgs: [],
+        description: "José Rizal's legacy is honored with a monument that serves as a tribute to his contributions to Filipino heritage, freedom, and nationalism. The site stands as a gathering place for the Filipino-American community, where celebrations and commemorations highlight his enduring influence on democracy and human rights.",
+        address: "7VP2+VX Singapore",
+        coordinates: [3.6578373629768572, 104.68740186636394]
+    },
+    {
+        title: "Juneau head statue - Manila Square in Alaska",
+        imgs: [],
+        description: "One of the happiest periods in José Rizal's life was his 45-day stay in Japan from February 28 to April 13, 1888. He arrived in Yokohama on February 18 and later stayed at the Tokyo Hotel before moving to the Spanish Legation. Fascinated by Japanese culture, he immersed himself in its language, traditions, and arts. He admired the country's natural beauty, the politeness of its people, and the charm of Japanese women, even falling in love with Seiko Usui. A bust of Rizal now stands in Hibiya Park, marking the site of the Tokyo Hotel where he once stayed.",
+        address: "7VP2+VX Singapore",
+        coordinates: [3.6578373629768572, 104.68740186636394]
+    },
+    {
+        title: "Rizal Statue - Herman Park, Houston, Texas",
+        imgs: [],
+        description: "One of the happiest periods in José Rizal's life was his 45-day stay in Japan from February 28 to April 13, 1888. He arrived in Yokohama on February 18 and later stayed at the Tokyo Hotel before moving to the Spanish Legation. Fascinated by Japanese culture, he immersed himself in its language, traditions, and arts. He admired the country's natural beauty, the politeness of its people, and the charm of Japanese women, even falling in love with Seiko Usui. A bust of Rizal now stands in Hibiya Park, marking the site of the Tokyo Hotel where he once stayed.",
         address: "7VP2+VX Singapore",
         coordinates: [3.6578373629768572, 104.68740186636394]
     },
