cb2cca4a628464ce7316a7e49fed952ff8f2b25b
Timan AlghieVic <timanalghievic@gmail.com>
2025-03-27 09:05:49 +0800

    team-Module

---
+++ app/Team/page.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Team() {
+  return (
+    <div className="relative min-h-screen w-full flex items-center justify-center">
+      <div className="container mx-auto p-6 w-full h-screen relative">
+        <h1 className="text-5xl text-center font-bold text-black mb-5">
+          Pepetons Teams
+        </h1>
+        <p className="text-4x1 text-center font-bold text-black mb-12">
+          Meet the team and learn more about the origin story behind Pepetones
+        </p>
+      </div>
+    </div>
+  );
+}
