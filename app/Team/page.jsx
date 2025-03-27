322ebf08b3c12fdab42e9fd8f7fa0fddb153a621
Timan AlghieVic <timanalghievic@gmail.com>
2025-03-27 12:41:56 +0800

    Team-Module 03/27/2025

--- app/Team/page.jsx
+++ app/Team/page.jsx
@@ -1,15 +1,36 @@
+"use client";
 import React from "react";
+import Image from "next/image";
+import TeamPhoto from "../../assets/Resources/TeamPhoto.jpg";
 
 export default function Team() {
   return (
-    <div className="relative min-h-screen w-full flex items-center justify-center">
-      <div className="container mx-auto p-6 w-full h-screen relative">
-        <h1 className="text-5xl text-center font-bold text-black mb-5">
-          Pepetons Teams
+    <div className="relative min-h-screen w-full flex flex-col items-center justify-center bg-white">
+      <div className="container mx-auto p-6 w-full max-w-5xl text-center">
+        {/* Heading */}
+        <h1 className="text-5xl font-extrabold text-black mb-3">
+          <span className="text-[#000]">Pepe</span>
+          <span className="text-[#000]">Toons</span> Team
         </h1>
-        <p className="text-4x1 text-center font-bold text-black mb-12">
-          Meet the team and learn more about the origin story behind Pepetones
+
+        {/* Subtitle */}
+        <p className="text-xl text-gray-600 mb-8">
+          Meet the team and learn more about the origin story behind{" "}
+          <span className="font-semibold text-black">PepeToons</span>
         </p>
+
+        {/* Image Section */}
+        <div className="relative w-full flex justify-center">
+          <div className="rounded-xl overflow-hidden shadow-lg">
+            <Image
+              src={TeamPhoto}
+              alt="Team Photo"
+              width={800} // Adjust for proper scaling
+              height={500}
+              className="rounded-lg"
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
