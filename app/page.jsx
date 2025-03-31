c9ad792147b1ce1b215d50dc573261804a5d85d4
eielelle <144611397+eielelle@users.noreply.github.com>
2025-03-31 10:16:04 +0800

    added animation

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,29 @@
+'use client'
+
 import Link from "next/link";
+import anime from 'animejs'
+import { useEffect } from "react";
 
 export default function Home() {
+
+  useEffect(() => {
+    anime({
+      targets: ".popup",
+      translateY: [20, 0],
+      opacity: [0, 1],
+      duration: 1000,
+      easing: 'easeInOutQuad',
+      delay: anime.stagger(200)
+    })
+
+    anime({
+      targets: ".slide",
+      maxWidth: ["100%", "0%"],
+      duration: 1000,
+      easing: 'easeInOutQuad',
+    })
+  }, [])
+
   return (
     <main className="h-screen w-screen relative">
       <video
@@ -17,17 +40,19 @@ export default function Home() {
       {/* content */}
       <div className="fixed top-0 left-0 h-full w-full flex flex-col justify-center items-center">
         <div className="flex flex-col w-1/2 text-center gap-4 mt-auto">
-          <h1 className="text-lg md:text-4xl">
+          <h1 className="text-lg md:text-4xl popup">
             PREPARE TO EMBARK ON A JOURNEY THROUGH HISTORY
           </h1>
-          <h1 className="text-xl md:text-5xl font-bold bg-white text-black p-4 inline-block">
+          <h1 className="text-xl md:text-5xl font-bold bg-white text-black p-4 inline-block relative">
             DISCOVER THE LIFE, IDEALS, AND LEGACY OF DR. JOSE RIZAL ...
+
+            <div className="slide absolute top-0 right-0 size-full bg-white"></div>
           </h1>
           <div className="flex justify-center items-center gap-4 flex-wrap">
-            <Link className="btn btn-white btn-outline md:btn-lg grow-0" href={"/museum-preview"}>
+            <Link className="btn btn-white btn-outline md:btn-lg grow-0 popup" href={"/museum-preview"}>
               Visit Museum
             </Link>
-            <Link className="btn btn-primary md:btn-lg grow-0" href={"/home"}>
+            <Link className="btn btn-primary md:btn-lg grow-0 popup" href={"/home"}>
               Explore Site
             </Link>
           </div>
