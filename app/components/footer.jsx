f970d74ea923c2024e45a21e7c81a02402d2152d
rekji11 <jerickdeguzman1030@gmail.com>
2025-04-02 09:27:32 +0800

    fix routing

--- app/components/footer.jsx
+++ app/components/footer.jsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
 <footer className="footer sm:footer-horizontal bg-base-200 text-base-content p-10 border-t-4 border-primary">
   <aside>
-    <span className="bg-primary text-white text-2xl font-bold p-2 rounded">pepetoons</span>
+    <a className="bg-primary text-white text-2xl font-bold p-2 rounded" href="/" >pepetoons</a>
     <p>
       <br />
       All Rights Reserved 2025
@@ -10,23 +10,23 @@ export default function Footer() {
   </aside>
   <nav>
     <h6 className="footer-title">About Pepetoons</h6>
-    <a className="link link-hover">Home</a>
-    <a className="link link-hover">Explore the Museum</a>
-    <a className="link link-hover">About</a>
+    <a className="link link-hover" href="/home">Home</a>
+    <a className="link link-hover" >Explore the Museum</a>
+    <a className="link link-hover" href="/about">About</a>
     <a className="link link-hover">Meet the Team</a>
   </nav>
   <nav>
     <h6 className="footer-title">Biography</h6>
-    <a className="link link-hover">Arrest</a>
-    <a className="link link-hover">Education</a>
-    <a className="link link-hover">Monuments</a>
-    <a className="link link-hover">Family</a>
+    <a className="link link-hover" href="/arrest" >Arrest</a>
+    <a className="link link-hover" href="/education" >Education</a>
+    <a className="link link-hover" href="/exile">Monuments</a>
+    <a className="link link-hover" href="/family">Family</a>
   </nav>
   <nav>
     <h6 className="footer-title opacity-0">A</h6>
-    <a className="link link-hover">Writings</a>
-    <a className="link link-hover">Romantic Life</a>
-    <a className="link link-hover">Travels</a>
+    <a className="link link-hover" href="/works">Writings</a>
+    <a className="link link-hover" href="/romantic-life">Romantic Life</a>
+    <a className="link link-hover" href="/visit">Travels</a>
   </nav>
 </footer>
   );
