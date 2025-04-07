d012db51e24de0261baf88ac592d14f325696d61
XeLwaren13 <axelwarenoleno@gmail.com>
2025-04-07 12:47:54 +0800

    Add back button, hover zoom grid effect

--- app/visit/europe/EuropeGrid.jsx
+++ app/visit/europe/EuropeGrid.jsx
@@ -15,18 +15,20 @@ const EuropeGrid = ({
   onOpen,
 }) => {
   return (
-    <div className="relative grid size-full max-sm:h-100 max-[850px]:h-100 bg-black hover:opacity-90">
-      <Image
-        src={image}
-        fill
-        className="relative object-cover size-full opacity-50"
-        alt={`${city}, ${country}`}
-      />
-      <h3 className="relative content-center text-center font-semibold text-4xl italic text-white w-full">
+    <div className="relative group overflow-hidden">
+      <div className="relative grid size-full max-sm:h-100 max-[850px]:h-100">
+        <Image
+          src={image}
+          fill
+          className="bg-black opacity-65 object-cover transition-all duration-300 ease-in-out group-hover:scale-110 group-hover:opacity-90 cursor-default"
+          alt={`${city}, ${country}`}
+        />
+      </div>
+      <h3 className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center font-semibold text-4xl italic text-white z-100 cursor-default">
         {city}, <br /> {country}
       </h3>
       <button
-        className="absolute bottom-4 justify-self-center btn btn-sm outline-2 bg-white text-black outline-offset-0 hover:bg-black hover:text-white transform transition duration-75 hover:scale-110 hover:cursor-pointer hover:outline-white hover:outline-1 "
+        className="absolute bottom-4 left-0 right-0 justify-self-center btn btn-sm outline-2 bg-white text-black outline-offset-0 hover:bg-black hover:text-white transform transition duration-75 hover:scale-110 hover:cursor-pointer hover:outline-white hover:outline-1 "
         onClick={() =>
           onOpen(
             city,
