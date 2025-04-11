7da0cb57e7692aefe2349deb3570d93b2f07f595
eielelle <144611397+eielelle@users.noreply.github.com>
2025-04-11 08:54:00 +0800

    updated code

---
+++ app/video/[slug]/data.js
@@ -0,0 +1,117 @@
+export const data = [
+  {
+    title: "Episode 1: Ang Pagsilang ng Bayani",
+    description: "Description for episode 1",
+    link: "https://player.cloudinary.com/embed/?cloud_name=dkkgia3jj&public_id=qbjpljaqmelerq2py36e&profile=user",
+  },
+  {
+    title: "Episode 2: Title",
+    description: "Description for episode 2",
+    link: "",
+  },
+  {
+    title: "Episode 3: Title",
+    description: "Description for episode 3",
+    link: "",
+  },
+  {
+    title: "Episode 4: Title",
+    description: "Description for episode 4",
+    link: "",
+  },
+  {
+    title: "Episode 5: Title",
+    description: "Description for episode 5",
+    link: "",
+  },
+  {
+    title: "Episode 6: Title",
+    description: "Description for episode 6",
+    link: "",
+  },
+  {
+    title: "Episode 7: Title",
+    description: "Description for episode 7",
+    link: "",
+  },
+  {
+    title: "Episode 8: Title",
+    description: "Description for episode 8",
+    link: "",
+  },
+  {
+    title: "Episode 9: Title",
+    description: "Description for episode 9",
+    link: "",
+  },
+  {
+    title: "Episode 10: Title",
+    description: "Description for episode 10",
+    link: "",
+  },
+  {
+    title: "Episode 11: Title",
+    description: "Description for episode 11",
+    link: "",
+  },
+  {
+    title: "Episode 12: Title",
+    description: "Description for episode 12",
+    link: "",
+  },
+  {
+    title: "Episode 13: Title",
+    description: "Description for episode 13",
+    link: "",
+  },
+  {
+    title: "Episode 14: Title",
+    description: "Description for episode 14",
+    link: "",
+  },
+  {
+    title: "Episode 15: Title",
+    description: "Description for episode 15",
+    link: "",
+  },
+  {
+    title: "Episode 16: Title",
+    description: "Description for episode 16",
+    link: "",
+  },
+  {
+    title: "Episode 17: Title",
+    description: "Description for episode 17",
+    link: "",
+  },
+  {
+    title: "Episode 18: Title",
+    description: "Description for episode 18",
+    link: "",
+  },
+  {
+    title: "Episode 19: Title",
+    description: "Description for episode 19",
+    link: "",
+  },
+  {
+    title: "Episode 20: Title",
+    description: "Description for episode 20",
+    link: "",
+  },
+  {
+    title: "Episode 21: Title",
+    description: "Description for episode 21",
+    link: "",
+  },
+  {
+    title: "Episode 22: Title",
+    description: "Description for episode 22",
+    link: "",
+  },
+  {
+    title: "Episode 23: Title",
+    description: "Description for episode 23",
+    link: "",
+  },
+];
---
+++ app/video/[slug]/page.jsx
@@ -0,0 +1,106 @@
+"use client";
+import { use } from "react";
+import { data } from "./data";
+import HeaderLayout from "../../../app/layouts/HeaderLayout";
+import Link from "next/link";
+
+// By default, the CldImage component applies auto-format and auto-quality to all delivery URLs for optimized delivery.
+export default function Page({ params }) {
+  const { slug } = use(params);
+
+  return (
+    <HeaderLayout>
+      <main className="mt-20 container mx-auto min-h-screen grid grid-cols-3 gap-6">
+        <section className="col-span-2">
+          <iframe
+            src={data[slug].link}
+            className="w-full aspect-video rounded-2xl border"
+          ></iframe>
+          <h1 className="my-4 text-4xl font-bold">{data[slug].title}</h1>
+          <h2 className="font-semibold">Description</h2>
+          <p>{data[slug].description}</p>
+        </section>
+        <section>
+          <ul className="list bg-base-100 rounded-box shadow-md">
+            <li className="p-4 pb-2 tracking-wide">
+              <h2 className="text-2xl font-bold">Up Next:</h2>
+            </li>
+
+            <li className="list-row">
+              <div>
+                <img
+                  className="size-10 rounded-box"
+                  src={`/thumbs/episode-${Number(slug) + 1}.png`}
+                />
+              </div>
+              <div>
+                <div>{data[Number(slug) + 1].title}</div>
+                <div className="text-xs uppercase font-semibold opacity-60">
+                  {data[Number(slug) + 1].description.slice(0, 15) + "..."}
+                </div>
+              </div>
+              <Link href={`/video/${Number(slug) + 1}`} className="btn btn-square btn-ghost">
+                <svg
+                  className="size-[1.2em]"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                >
+                  <g
+                    strokeLinejoin="round"
+                    strokeLinecap="round"
+                    strokeWidth="2"
+                    fill="none"
+                    stroke="currentColor"
+                  >
+                    <path d="M6 3L20 12 6 21 6 3z"></path>
+                  </g>
+                </svg>
+              </Link>
+            </li>
+          </ul>
+
+          <div className="divider"></div>
+
+          <ul className="list rounded-box">
+
+          {data.slice(Number(slug) + 2).map((val, index) => (
+            <li key={index} className="list-row">
+              <div>
+                <img
+                  className="w-[200px] object-cover aspect-video rounded-box"
+                  src={`/thumbs/episode-${Number(slug) + index + 2}.png`}
+                />
+              </div>
+              <div>
+                <div>{val.title}</div>
+                <div className="text-xs uppercase font-semibold opacity-60">
+                  {val.description.slice(0, 30) + "..."}
+                </div>
+              </div>
+              <Link href={`/video/${Number(slug) + index + 2}`} className="btn btn-square btn-ghost">
+                <svg
+                  className="size-[1.2em]"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                >
+                  <g
+                    strokeLinejoin="round"
+                    strokeLinecap="round"
+                    strokeWidth="2"
+                    fill="none"
+                    stroke="currentColor"
+                  >
+                    <path d="M6 3L20 12 6 21 6 3z"></path>
+                  </g>
+                </svg>
+              </Link>
+            </li>
+          ))}
+
+          </ul>
+
+        </section>
+      </main>
+    </HeaderLayout>
+  );
+}
