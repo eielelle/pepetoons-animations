4e505356c8114500b64315705e4f93a59a79c704
eielelle <144611397+eielelle@users.noreply.github.com>
2025-04-15 08:45:51 +0800

    updated description

--- app/video/[slug]/data.js
+++ app/video/[slug]/data.js
@@ -16,7 +16,7 @@ export const data = [
   },
   {
     title: "Episode 4: Ang Pagbibinata",
-    description: "Description for episode 4",
+    description: "Ang makulay na kuwento ng paglaki ni Jose Rizal bilang isang pambansang bayani ng Pilipinas ay tinatawag na Ang Pagbibinata ni Jose Rizal. Pinapakita nito ang kanyang mga unang taon bilang isang bata ang kanyang pagmamalasakit sa kanyang pamilya at ang kanyang pagsisikap na matuto. Sa bawat hakbang ang kanyang mga tagumpay at mga pagsubok ay nagbibigay-daan sa kanya na magpasya kung ano ang gusto niyang gawin. Ang animasyon na ito ay nagbibigay ng inspirasyon at determinasyon na naging pundasyon ng kanyang dakilang misyon sa hinaharap mula sa kanyang mga simpleng laro bilang isang bata hanggang sa mahahalagang aral na ibinibigay sa mga kabataan.",
     link: "",
   },
   {
@@ -36,22 +36,22 @@ export const data = [
   },
   {
     title: "Episode 8: Ang Paglalayag Paalis ng Bansa",
-    description: "Description for episode 8",
+    description: "Sa Episode na ito pinapakita ang isang kahanga-hangang kuwento ng paglalakbay ni Rizal sa ibang bansa upang matuto nang higit pa at matupad ang kanyang mga pangarap. Sa panahon ng kanyang paglalakbay ipinakita niya ang kanyang matinding pagsisikap na suportahan ang kanyang bayan at ang kanyang katapangan sa harap ng mga hindi inaasahang hamon. Ang animasyong ito ay tumutuon sa mga bagay na nalaman ni Rizal sa kanyang paglalakbay at kung paano ito nakaimpluwensya sa kanyang mga pananaw at mga layunin sa buhay.",
     link: "",
   },
   {
     title: "Episode 9: Madrid",
-    description: "Description for episode 9",
+    description: "Ang buhay ni Jose Rizal sa Madrid ang kabisera ng Espanya ay pinag-uusapan. Ang animasyong ito ay nagpapakita ng kanyang masigasig na pag-aaral sa Universidad Central de Madrid ang kanyang pakikilahok sa mga talakayan ng mga Pilipinong repormista at ang pagsisimula ng kanyang pagsulat ng mga akdang magpapabago sa kasaysayan. Bilang karagdagan ipinapakita dito ang mga paghihirap na nararanasan ni Rizal sa Madrid kabilang ang pag-adjust sa isang bagong kultura at ang kanyang mga pagsisikap na maisulong ang pagbabago sa Pilipinas.",
     link: "",
   },
   {
     title: "Episode 10: Doktor at Manlalakbay",
-    description: "Description for episode 10",
+    description: "Sa episode na ito ipinapakita ang isang nakakaantig na kuwento na nagpapakita ng dalawang pangunahing bahagi ng buhay ni Jose Rizal: ang kanyang karera bilang isang manggagamot at ang kanyang walang katapusang layunin na maglakbay upang matuto at maglingkod. Sa animasyong ito ipinapakita kung paano ginamit ni Rizal ang kanyang kaalaman sa medisina upang tumulong sa mga nangangailangan habang naglalakbay sa buong mundo upang malaman ang tungkol sa ibat ibang ideya at kultura. Isang kwento ng pagiging dedikado serbisyo at paglalakbay patungo sa isang mas mataas na layunin.",
     link: "",
   },
   {
     title: "Episode 11: Ang Unang Nobela",
-    description: "Description for episode 11",
+    description: "Ang Unang Nobela ni Rizal ay tungkol sa kung paano nagsimula si Jose Rizal bilang isang makata at manunulat na may layuning ipakita ang mga problema sa lipunan sa panahon niya. Ang animasyong ito ay nagsasalaysay ng mga pangyayari na nag-udyok sa kanya na magsulat ng Noli Me Tangere kabilang ang kanyang inspirasyon ang kanyang mga hamon sa proseso at ang kanyang matapang na desisyon na ipakita ang kawalang-katarungan at maling gawa sa kolonyal na Pilipinas sa mundo. Ipinapakita din kung paano binago ng nobelang ito ang landas ng kasaysayan at naging simbolo ng pagkilos tungo sa kalayaan.",
     link: "",
   },
   {
@@ -101,17 +101,17 @@ export const data = [
   },
   {
     title: "Episode 21: Title",
-    description: "Description for episode 21",
+    description: "Sa episode na ito ng kwento ni Dr. Jose Rizal, masisilayan natin ang huling mga araw ni Rizal sa Dapitan—isang yugto ng kanyang buhay na puno ng katahimikan, lungkot, at matibay na prinsipyo. Sa kabila ng mga alok ng pagtakas mula sa mga kaibigan, pamilya, at maging mga kaalyadong Muslim, nanindigan si Rizal sa kanyang paninindigang hindi tatakas. Makikita ang lalim ng kanyang damdamin habang nagpapaalam siya sa mga mahal sa buhay, ibinabahagi ang mga alaala, at iniiwan ang mga simpleng kayamanan bilang alaala sa mga estudyante. \nSa kanyang tahimik ngunit matapang na pag-alis—kasama sina Josephine, Lola Sisa, at ilang piling kasamahan—nagsimula ang panibagong kabanata ng kanyang buhay. Ngunit habang lumalayo ang kanyang barko mula sa baybayin ng Dapitan, nababalot ito ng isang babala: ang kalayaang inaasam ay maaaring may kapalit na kapalarang mapait.",
     link: "",
   },
   {
     title: "Episode 22: Title",
-    description: "Description for episode 22",
+    description: "Sa yugtong ito ng buhay ni Dr. Jose Rizal, sumilip tayo sa isa sa mga pinakamadilim at pinakamatapang na kabanata ng kanyang pakikibaka — ang kanyang pagkakaaresto, paglilitis, at pagharap sa hatol ng kamatayan. Habang patungo sana sa Espanya, si Rizal ay inaresto at ibinalik sa Pilipinas upang litisin sa kasong sedisyon at rebelyon. Sa kabila ng maraming pagkakataong makatakas, pinili niyang manatili at harapin ang paratang — dala ang paninindigang wala siyang kasalanan at tapat ang kanyang layunin para sa bayan. Dito rin masisilayan ang sakripisyo ng kanyang kapatid na si Paciano, ang paghihirap ng kanyang pamilya, at ang kanyang tahimik ngunit matibay na paninindigan sa harap ng lumulupig na kapangyarihan. Sa kabila ng lahat, isinulat ni Rizal ang kanyang 'Manifesto sa mga Pilipino', mariing tinanggihan ang marahas na rebolusyon, ngunit piniling mamuhay at mamatay para sa dangal at katotohanan. Isang makapangyarihang yugto ng kasaysayan — ang paglilitis ng isang dakilang bayani na sa halip na yumuko sa takot, ay mas piniling ipaglaban ang katwiran, kahit kapalit ay sariling buhay.",
     link: "",
   },
   {
     title: "Episode 23: Title",
-    description: "Description for episode 23",
+    description: "Sa huling gabi ng kanyang buhay, muling humarap si Dr. Jose Rizal sa pinakamalalim na tanong tungkol sa kanyang paninindigan at pananampalataya. Ang episode na ito ay sumasalamin sa emosyonal, moral, at espiritwal na bagyong kanyang hinarap bago ang pagbitay—isang yugto na patuloy na pinagtatalunan ng kasaysayan. Sa kabila ng mahigpit na bantay, may mga nagsasabing nagbalik-loob si Rizal sa simbahan, nagkumpisal, at ikinasal kay Josephine Bracken. Ngunit tinututulan ito ng ilan, kabilang ang kanyang kapatid na si Paciano, na nanindigang hindi niya tinalikuran ang mga prinsipyo ng kanyang pakikibaka. Makikita rin sa episode ang kanyang huling sandali sa kulungan: ang pagsusulat ng Mi Último Adiós, ang kanyang mga liham para sa pamilya at mga kaibigan, at ang payapang pagtanggap sa kamatayan. Sa umagang iyon ng Disyembre 30, 1896, si Rizal ay naglakad patungong Bagumbayan—hindi bilang takas, kundi bilang isang bayani na tahimik ngunit buong tapang na hinarap ang kanyang kapalaran. “Bagyo at Pagsubok” ay isang masinsinang pagtanaw sa puso't isip ng ating pambansang bayani sa huling sandali ng kanyang buhay—isang paalala na ang tunay na kabayanihan ay hindi lamang nasusukat sa sigaw ng rebolusyon, kundi sa katahimikan ng paninindigan.",
     link: "",
   },
 ];
--- app/video/[slug]/page.jsx
+++ app/video/[slug]/page.jsx
@@ -8,6 +8,10 @@ import Link from "next/link";
 export default function Page({ params }) {
   const { slug } = use(params);
 
+  function constraintIndex(num) {
+    return num < data.length;
+  }
+
   return (
     <HeaderLayout>
       <main className="mt-20 p-4 container mx-auto min-h-screen grid grid-cols-1 xl:grid-cols-3 gap-6">
@@ -21,84 +25,89 @@ export default function Page({ params }) {
           <p>{data[slug].description}</p>
         </section>
         <section className="h-screen flex flex-col mb-6">
-          <ul className="list bg-base-100 rounded-box shadow-md">
-            <li className="p-4 pb-2 tracking-wide">
-              <h2 className="text-2xl font-bold">Up Next:</h2>
-            </li>
+          {constraintIndex(Number(slug) + 1) && (
+            <ul className="list bg-base-100 rounded-box shadow-md">
+              <li className="p-4 pb-2 tracking-wide">
+                <h2 className="text-2xl font-bold">Up Next:</h2>
+              </li>
 
-            <li className="list-row">
-              <div>
-                <img
-                  className="size-10 rounded-box"
-                  src={`/thumbs/episode-${Number(slug) + 2}.png`}
-                />
-              </div>
-              <div>
-                <div>{data[Number(slug) + 1].title}</div>
-                <div className="text-xs uppercase font-semibold opacity-60">
-                  {data[Number(slug) + 1].description.slice(0, 15) + "..."}
+              <li className="list-row">
+                <div>
+                  <img
+                    className="size-10 rounded-box"
+                    src={`/thumbs/episode-${Number(slug) + 2}.png`}
+                  />
                 </div>
-              </div>
-              <Link href={`/video/${Number(slug) + 1}`} className="btn btn-square btn-ghost">
-                <svg
-                  className="size-[1.2em]"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
+                <div>
+                  <div>{data[Number(slug) + 1].title}</div>
+                  <div className="text-xs uppercase font-semibold opacity-60">
+                    {data[Number(slug) + 1].description.slice(0, 15) + "..."}
+                  </div>
+                </div>
+                <Link
+                  href={`/video/${Number(slug) + 1}`}
+                  className="btn btn-square btn-ghost"
                 >
-                  <g
-                    strokeLinejoin="round"
-                    strokeLinecap="round"
-                    strokeWidth="2"
-                    fill="none"
-                    stroke="currentColor"
+                  <svg
+                    className="size-[1.2em]"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
                   >
-                    <path d="M6 3L20 12 6 21 6 3z"></path>
-                  </g>
-                </svg>
-              </Link>
-            </li>
-          </ul>
+                    <g
+                      strokeLinejoin="round"
+                      strokeLinecap="round"
+                      strokeWidth="2"
+                      fill="none"
+                      stroke="currentColor"
+                    >
+                      <path d="M6 3L20 12 6 21 6 3z"></path>
+                    </g>
+                  </svg>
+                </Link>
+              </li>
+            </ul>
+          )}
 
           <div className="divider"></div>
 
           <ul className="list rounded-box flex-1 overflow-scroll">
-
-          {data.slice(Number(slug) + 2).map((val, index) => (
-            <li key={index} className="list-row">
-              <div>
-                <img
-                  className="w-[200px] object-cover aspect-video rounded-box"
-                  src={`/thumbs/episode-${Number(slug) + index + 3}.png`}
-                />
-              </div>
-              <div>
-                <div>{val.title}</div>
-                <div className="text-xs uppercase font-semibold opacity-60">
-                  {val.description.slice(0, 30) + "..."}
+            {data.slice(Number(slug) + 2).map((val, index) => (
+              <li key={index} className="list-row">
+                <div>
+                  <img
+                    className="w-[200px] object-cover aspect-video rounded-box"
+                    src={`/thumbs/episode-${Number(slug) + index + 3}.png`}
+                  />
                 </div>
-              </div>
-              <Link href={`/video/${Number(slug) + index + 2}`} className="btn btn-square btn-ghost">
-                <svg
-                  className="size-[1.2em]"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
+                <div>
+                  <div>{val.title}</div>
+                  <div className="text-xs uppercase font-semibold opacity-60">
+                    {val.description.slice(0, 30) + "..."}
+                  </div>
+                </div>
+                <Link
+                  href={`/video/${Number(slug) + index + 2}`}
+                  className="btn btn-square btn-ghost"
                 >
-                  <g
-                    strokeLinejoin="round"
-                    strokeLinecap="round"
-                    strokeWidth="2"
-                    fill="none"
-                    stroke="currentColor"
+                  <svg
+                    className="size-[1.2em]"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
                   >
-                    <path d="M6 3L20 12 6 21 6 3z"></path>
-                  </g>
-                </svg>
-              </Link>
-            </li>
-          ))}
-
+                    <g
+                      strokeLinejoin="round"
+                      strokeLinecap="round"
+                      strokeWidth="2"
+                      fill="none"
+                      stroke="currentColor"
+                    >
+                      <path d="M6 3L20 12 6 21 6 3z"></path>
+                    </g>
+                  </svg>
+                </Link>
+              </li>
+            ))}
           </ul>
-
         </section>
       </main>
     </HeaderLayout>
