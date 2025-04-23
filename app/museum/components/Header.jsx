8f1b9dfb7b8e9be15f6d8589ca582f493ab40cd7
rekji11 <jerickdeguzman1030@gmail.com>
2025-04-23 08:40:47 +0800

    added x when eye icon is toggled

--- app/museum/components/Header.jsx
+++ app/museum/components/Header.jsx
@@ -1,20 +1,43 @@
+import {useState} from "react";
 import Image from "next/image";
-import { Home, Eye } from "react-feather";
+import { Home, Eye, X } from "react-feather";
 import Link from 'next/link'
 
 export default function Header({hide}) {
-    return <header className="navbar">
-    <div className="flex-1">
-      <Image src={"/logo.png"} alt={"logo"} width={150} height={150} />
-    </div>
-    <div className="flex-none flex gap-4">
-      <button className="btn btn-square bg-[#C66030]" onClick={hide}>
-        <Eye />
-      </button>
+  const [isHidden, setIsHidden] = useState(false);
 
-      <Link href="/home" className="btn btn-square bg-[#C66030]">
+  const toggleVisibility = () => {
+    setIsHidden(!isHidden);
+    hide();
+  };
+
+  return (
+    <header className="navbar">
+      <div className="flex-1">
+        <Image src={"/logo.png"} alt={"logo"} width={150} height={150} />
+      </div>
+      <div className="flex-none flex gap-4">
+        <button className="btn btn-square bg-[#C66030]" onClick={toggleVisibility}>{isHidden ? <X /> : <Eye />}
+        </button>
+        <Link href="/home" className="btn btn-square bg-[#C66030]">
         <Home />
-      </Link>
-    </div>
-  </header>
+        </Link>
+      </div>
+    </header>
+  );
 }
+//     return <header className="navbar">
+//     <div className="flex-1">
+//       <Image src={"/logo.png"} alt={"logo"} width={150} height={150} />
+//     </div>
+//     <div className="flex-none flex gap-4">
+//       <button className="btn btn-square bg-[#C66030]" onClick={hide}>
+//         <Eye />
+//       </button>
+
+//       <Link href="/home" className="btn btn-square bg-[#C66030]">
+//         <Home />
+//       </Link>
+//     </div>
+//   </header>
+// }
